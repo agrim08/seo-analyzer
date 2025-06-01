cc634e9e0212eba891fa36f13909727ab92c4b5e
agrim08 <agrimgupta0805@gmail.com>
2025-06-01 14:42:45 +0530

    bug fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -162,7 +162,7 @@ const Home = () => {
                     Analyzing
                   </>
                 ) : (
-                  <div className="cursor-pointer flex items-center justify-center gap-x-3">
+                  <div className="flex items-center justify-center gap-x-3">
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path
                         strokeLinecap="round"
